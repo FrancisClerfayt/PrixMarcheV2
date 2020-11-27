4cbd0d577ddf8249799e714b6cafa42e1edb01f6
FrancisClerfayt <francis.clerfayt@gmail.com>
2020-11-27 14:37:59 +0100

    added picture for Categories

--- resources/views/Category/edit.blade.php
+++ resources/views/Category/edit.blade.php
@@ -6,12 +6,16 @@
 <div class="container">
 	<div class="row justify-content-center">
 		<div class="col-sm-8">
-			<form action=" {{ route('Category.update', ['Category'=>$Category->id] ) }} " method="POST">
+			<form action="{{route('Category.update', ['Category'=>$Category->id])}}" method="POST">
 				@csrf
 				@method('PUT')
 				<div class="form-group">
 					<label for="name">Nom Catégorie</label>
-					<input type="text" class="form-control" id="name" name="name" value=" {{ $Category->name }} ">
+					<input value="{{$Category->name}}" class="form-control" type="text" id="name" name="name">
+				</div>
+				<div class="form-group">
+					<label for="picture">Chemin de l'icône</label>
+					<input value="{{$Category->picture}}" class="form-control" type="text" name="picture" id="picture">
 				</div>
 				<div class="form-group">
 					<input type="submit" value="Valider modification" class="btn btn-primary">
